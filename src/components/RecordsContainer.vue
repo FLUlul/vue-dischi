c9b2356ffcb2e489697be4aab1fb813e1bed9f4e
FLU <valerio.massj@gmail.com>
2021-11-26 14:14:15 +0100

    init and component parent RecordsContainer set up

---
+++ src/components/RecordsContainer.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="container">
+    <header>
+      <img src="../assets/spotify-logo.png" alt="logo spotify">
+    </header>
+
+    <main>
+
+      
+
+    </main>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RecordsContainer',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  header{
+    background-color: #2e3a46;
+    padding: 10px;
+
+    img{
+      width: 50px;
+    }
+  }
+  main{
+    background-color: #1e2d3b;
+    padding: 30px;
+  }
+</style>
